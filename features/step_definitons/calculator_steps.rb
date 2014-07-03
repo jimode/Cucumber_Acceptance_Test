27ea00f4285b9a0a890bc4c41bc055aaaaff4465
Jimi Odeyemi <jimode68@gmail.com>
2014-07-04 00:14:43 +0100

    add transform method

--- features/step_definitons/calculator_steps.rb
+++ features/step_definitons/calculator_steps.rb
@@ -6,6 +6,10 @@ When(/^I add (\d+) and (\d+)$/) do |arg1, arg2|
   @result = @calculator.add arg1.to_i, arg2.to_i
 end
 
-Then(/^the result should be (\d+)$/) do |arg1|
-  expect(@result).to eq(arg1.to_i)
+Then(/^the result should be (\d+)$/) do |expected|
+  expect(@result).to eq(expected)
+end
+
+Transform /^(\d+)$/ do |number|
+	number.to_i
 end
